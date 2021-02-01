744a613c0716c7925e0c8fb6303a5b50c90aa899
wuliaotc <372072553@qq.com>
2021-02-01 21:26:44 +0800

    update theme config

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,9 +1,12 @@
+# Defined interactively
 function fish_prompt --description 'Write out the prompt'
-    set laststatus $status
+    set -l laststatus $status
 
+    set -l git_info
     if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
         set git_branch (set_color -o blue)"$git_branch"
-        if command git diff-index --quiet HEAD --
+        set -l git_status
+        if not command git diff-index --quiet HEAD --
             if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
                 echo $count | read -l ahead behind
                 if test "$ahead" -gt 0
@@ -13,7 +16,7 @@ function fish_prompt --description 'Write out the prompt'
                     set git_status "$git_status"(set_color red)⬇
                 end
             end
-            for i in (git status --porcelain | string sub -l 2 | uniq)
+            for i in (git status --porcelain | string sub -l 2 | sort | uniq)
                 switch $i
                     case "."
                         set git_status "$git_status"(set_color green)✚
@@ -35,8 +38,12 @@ function fish_prompt --description 'Write out the prompt'
         set git_info "(git$git_status$git_branch"(set_color white)")"
     end
 
+    # Disable PWD shortening by default.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
     set_color -b black
-    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (echo $PWD | sed -e "s|^$HOME|~|") (set_color white) $git_info (set_color white) '❱' (set_color white)
+    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
     if test $laststatus -eq 0
         printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
     else
