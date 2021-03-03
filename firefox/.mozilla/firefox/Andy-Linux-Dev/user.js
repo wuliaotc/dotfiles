d45047b9f26ef54bf70fec700234845f49904d13
wuliaotc <372072553@qq.com>
2021-03-03 10:25:45 +0800

    use firefox theme akshat46/FlyingFox

--- firefox/.mozilla/firefox/Andy-Linux-Dev/user.js
+++ firefox/.mozilla/firefox/Andy-Linux-Dev/user.js
@@ -1,3 +1,7 @@
+/*** FlyingFox ****/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
+user_pref("svg.context-properties.content.enabled", true);
+
 // 目的是在firefox上实现物理的平滑滚动
 //
 
